c413b2b0fa65e21c255bdc066ebe369692bf51fa
davidUwUalja <davidnorthkorodi@gmail.com>
2025-01-27 12:18:35 +0100

    nyelv demo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
+import i18n from './i18n';
+
 
 createApp(App).use(router).mount('#app');
+pp.use(i18n);
+app.mount('#app');
