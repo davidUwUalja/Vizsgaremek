276a430705a8894abab27f90ed7e488c334418b7
Petz Dávid <petzdavid88@gmail.com>
2025-04-26 19:03:10 +0000

    Order updated

---
+++ backend/app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true; 
+    }
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'total_price' => 'required|integer|max:50',
+            'status' => 'sometimes|string|in:pending,completed,cancelled', 
+        ];
+    }
+}
--- backend/database/migrations/2025_02_17_115855_create_orders_table.php
+++ backend/database/migrations/2025_02_17_115855_create_orders_table.php
@@ -10,8 +10,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
-            $table->decimal('total_price');
+            $table->foreignId('user_id')->constrained(); 
+            $table->integer('total_price');
             $table->string('status')->default('pending'); 
             $table->timestamps();
         });
