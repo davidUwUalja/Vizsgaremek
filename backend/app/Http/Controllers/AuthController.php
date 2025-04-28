6b9af7597c114959d14a9c5ce798c4fa887dbc54
Kocsis András Barnabás <kocsibarni12@gmail.com>
2025-04-28 09:42:58 +0000

    Apróbb törlések

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -45,57 +45,5 @@ class AuthController extends Controller
             ]
         ]);
     }
-    /*
-    public function login(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required|min:6',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json([
-                'error' => 'Validation Error',
-                'message' => $validator->errors()
-            ], 422);
-        }
-
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json([
-                'error' => 'Unauthorized',
-                'message' => 'Invalid credentials'
-            ], 401);
-        }
-
-        $token = $user->createToken('regisegek')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Login successful',
-            'token' => $token
-        ]);
-    }
-        
-    
-    public function register(RegisterRequest $request)
-    {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        $token = $user->createToken('regisegek')->plainTextToken;
-
-        return response()->json([
-            'message' => 'Registration successful',
-            'token' => $token
-        ], 201);
-    }  
-    
-    */
-
-
 
 }
